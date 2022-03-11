3aaebaabf9609d53dbdaac8f2cd9fa6deb22a13d
Vitor Medeiros <vitorpaulo3121@gmail.com>
2022-03-11 01:45:51 -0300

    Sokme bug fixed

--- anime_list/lib/screens/profile_screen.dart
+++ anime_list/lib/screens/profile_screen.dart
@@ -25,6 +25,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   getAnimes(UserProfile userProfile) async {
     if (userListAnime.isNotEmpty) {
+      print("here");
       return;
     }
     setState(() {
@@ -32,12 +33,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
     });
     userListAnime = await Provider.of<AnimeList>(context, listen: false)
         .getAnimeListFromUserProfile(userProfile);
-    
+
     setState(() {
       isLoading = false;
     });
   }
-
+@override
+  void dispose() {
+    userListAnime = [];
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     UserProfile userProfile =
@@ -48,188 +53,196 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ?.id ??
             false);
     getAnimes(userProfile);
-    return isLoading
-        ? const Center(
-            child: CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          )
-        : Scaffold(
-            backgroundColor: Colors.black,
-            drawer: AppDrawer(),
-            body: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  pinned: true,
-                  // floating: true,
-                  // snap: true,
-                  expandedHeight: 350,
-                  backgroundColor: Colors.black54,
+    return WillPopScope(
+      onWillPop: () {
+        return Future.value(false);
+      },
+      child: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            )
+          : Scaffold(
+              backgroundColor: Colors.black,
+              drawer: AppDrawer(),
+              body: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    pinned: true,
+                    // floating: true,
+                    // snap: true,
+                    expandedHeight: 350,
+                    backgroundColor: Colors.black54,
 
-                  centerTitle: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    title: Text(
-                      userProfile.name,
-                    ),
                     centerTitle: true,
-                    collapseMode: CollapseMode.parallax,
-                    background: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        CircleAvatar(
-                          minRadius: 80,
-                          maxRadius: 100,
-                          foregroundImage:
-                              NetworkImage(userProfile.profileImageUrl),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: isSelfUser
-                                  ? const Icon(
-                                      Icons.favorite,
-                                      color: Colors.red,
-                                    )
-                                  : const Icon(Icons.favorite_border),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
-                                isSelfUser ? "Editar" : "Adicionar",
-                                style: TextStyle(
-                                  color:
-                                      Theme.of(context).colorScheme.onSecondary,
-                                ),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(92),
-                                    ),
-                                  ),
-                                  primary: isSelfUser
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Theme.of(context).colorScheme.secondary,
-                                  onPrimary: Colors.white),
-                            ),
-                            IconButton(
+                    flexibleSpace: FlexibleSpaceBar(
+                      title: Text(
+                        userProfile.name,
+                      ),
+                      centerTitle: true,
+                      collapseMode: CollapseMode.parallax,
+                      background: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          CircleAvatar(
+                            minRadius: 80,
+                            maxRadius: 100,
+                            foregroundImage:
+                                NetworkImage(userProfile.profileImageUrl),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
                                 onPressed: () {},
-                                icon: const Icon(
-                                  Icons.email_outlined,
-                                )),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: kToolbarHeight + 3,
-                        ),
-                      ],
+                                icon: isSelfUser
+                                    ? const Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      )
+                                    : const Icon(Icons.favorite_border),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  isSelfUser ? "Editar" : "Adicionar",
+                                  style: TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSecondary,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(92),
+                                      ),
+                                    ),
+                                    primary: isSelfUser
+                                        ? Theme.of(context).colorScheme.primary
+                                        : Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                    onPrimary: Colors.white),
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.email_outlined,
+                                  )),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: kToolbarHeight + 3,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SliverToBoxAdapter(
-                  child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                      child: Container(
-                        color: Colors.grey[850],
-                        child: Column(
-                          children: [
-                            TitledRowList(
-                              title: "Assistindo",
-                              hasArrow: true,
-                              listTipe: ListTipe.WATCHING,
-                              userList:
-                                  AnimeList(context, userList: userListAnime),
-                              onTap: () {
-                                Provider.of<AnimeList>(context, listen: false)
-                                    .getAnimeListFromUserProfile(userProfile)
-                                    .then((userList) {
-                                  Navigator.of(context).pushNamed(
-                                      AppRoutes.ANIME_LIST,
-                                      arguments: {
-                                        "listTipe": ListTipe.WATCHING,
-                                        "userList": AnimeList(context,
-                                            userList: userList)
-                                      });
-                                });
-                              },
-                            ),
-                            TitledRowList(
-                              title: "Prioridades",
-                              hasArrow: true,
-                              listTipe: ListTipe.PRIO,
-                              userList:
-                                  AnimeList(context, userList: userListAnime),
-                              onTap: () {
-                                Provider.of<AnimeList>(context, listen: false)
-                                    .getAnimeListFromUserProfile(userProfile)
-                                    .then((userList) {
-                                  Navigator.of(context).pushNamed(
-                                      AppRoutes.ANIME_LIST,
-                                      arguments: {
-                                        "listTipe": ListTipe.PRIO,
-                                        "userList": AnimeList(context,
-                                            userList: userList)
-                                      });
-                                });
-                              },
-                            ),
-                            TitledRowList(
-                              title: "Para Assistir",
-                              hasArrow: true,
-                              listTipe: ListTipe.NORMAL,
-                              userList:
-                                  AnimeList(context, userList: userListAnime),
-                              onTap: () {
-                                Provider.of<AnimeList>(context, listen: false)
-                                    .getAnimeListFromUserProfile(userProfile)
-                                    .then((userList) {
-                                  Navigator.of(context).pushNamed(
-                                      AppRoutes.ANIME_LIST,
-                                      arguments: {
-                                        "listTipe": ListTipe.NORMAL,
-                                        "userList": AnimeList(context,
-                                            userList: userList)
-                                      });
-                                });
-                              },
-                            ),
-                            TitledRowList(
-                              title: "Concluidos",
-                              hasArrow: true,
-                              listTipe: ListTipe.FINISHED,
-                              userList:
-                                  AnimeList(context, userList: userListAnime),
-                              onTap: () {
-                                Provider.of<AnimeList>(context, listen: false)
-                                    .getAnimeListFromUserProfile(userProfile)
-                                    .then((userList) {
-                                  Navigator.of(context).pushNamed(
-                                      AppRoutes.ANIME_LIST,
-                                      arguments: {
-                                        "listTipe": ListTipe.FINISHED,
-                                        "userList": AnimeList(context,
-                                            userList: userList)
-                                      });
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20,
-                            )
-                          ],
+                  SliverToBoxAdapter(
+                    child: ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
                         ),
-                      )),
-                )
-              ],
+                        child: Container(
+                          color: Colors.grey[850],
+                          child: Column(
+                            children: [
+                              TitledRowList(
+                                title: "Assistindo",
+                                hasArrow: true,
+                                listTipe: ListTipe.WATCHING,
+                                userList:
+                                    AnimeList(context, userList: userListAnime),
+                                onTap: () {
+                                  Provider.of<AnimeList>(context, listen: false)
+                                      .getAnimeListFromUserProfile(userProfile)
+                                      .then((userList) {
+                                    Navigator.of(context).pushNamed(
+                                        AppRoutes.ANIME_LIST,
+                                        arguments: {
+                                          "listTipe": ListTipe.WATCHING,
+                                          "userList": AnimeList(context,
+                                              userList: userList)
+                                        });
+                                  });
+                                },
+                              ),
+                              TitledRowList(
+                                title: "Prioridades",
+                                hasArrow: true,
+                                listTipe: ListTipe.PRIO,
+                                userList:
+                                    AnimeList(context, userList: userListAnime),
+                                onTap: () {
+                                  Provider.of<AnimeList>(context, listen: false)
+                                      .getAnimeListFromUserProfile(userProfile)
+                                      .then((userList) {
+                                    Navigator.of(context).pushNamed(
+                                        AppRoutes.ANIME_LIST,
+                                        arguments: {
+                                          "listTipe": ListTipe.PRIO,
+                                          "userList": AnimeList(context,
+                                              userList: userList)
+                                        });
+                                  });
+                                },
+                              ),
+                              TitledRowList(
+                                title: "Para Assistir",
+                                hasArrow: true,
+                                listTipe: ListTipe.NORMAL,
+                                userList:
+                                    AnimeList(context, userList: userListAnime),
+                                onTap: () {
+                                  Provider.of<AnimeList>(context, listen: false)
+                                      .getAnimeListFromUserProfile(userProfile)
+                                      .then((userList) {
+                                    Navigator.of(context).pushNamed(
+                                        AppRoutes.ANIME_LIST,
+                                        arguments: {
+                                          "listTipe": ListTipe.NORMAL,
+                                          "userList": AnimeList(context,
+                                              userList: userList)
+                                        });
+                                  });
+                                },
+                              ),
+                              TitledRowList(
+                                title: "Concluidos",
+                                hasArrow: true,
+                                listTipe: ListTipe.FINISHED,
+                                userList:
+                                    AnimeList(context, userList: userListAnime),
+                                onTap: () {
+                                  Provider.of<AnimeList>(context, listen: false)
+                                      .getAnimeListFromUserProfile(userProfile)
+                                      .then((userList) {
+                                    Navigator.of(context).pushNamed(
+                                        AppRoutes.ANIME_LIST,
+                                        arguments: {
+                                          "listTipe": ListTipe.FINISHED,
+                                          "userList": AnimeList(context,
+                                              userList: userList)
+                                        });
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                height: 20,
+                              )
+                            ],
+                          ),
+                        )),
+                  )
+                ],
+              ),
             ),
-          );
+    );
   }
 }
