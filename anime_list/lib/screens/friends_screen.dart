3a81a050c78f835a5f04436ca1d7bcad1b13ab1e
vitor <vitorpaulo3121@gmail.com>
2022-04-16 12:03:24 -0300

    frinds Screen

--- anime_list/lib/screens/friends_screen.dart
+++ anime_list/lib/screens/friends_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class friendsScreen extends StatefulWidget {
@@ -10,6 +11,35 @@ class friendsScreen extends StatefulWidget {
 class _friendsScreenState extends State<friendsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double avaliableScreenSpace = MediaQuery.of(context).size.height -
+        (kToolbarHeight +
+            MediaQuery.of(context).padding.bottom +
+            MediaQuery.of(context).padding.top);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Amigos"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.group_add),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.5),
+            alignment: Alignment.center,
+            height: avaliableScreenSpace * 0.15,
+            width: double.infinity,
+            child: CupertinoTextField(),
+          )
+        ],
+      ),
+    );
   }
 }
